b80688cb0c5fee4a24943b002d855e11b993df48
Aras14HD <aras14k5@gmail.com>
2024-07-11 07:57:12 +0200

    update nix package to 1.1.0

--- default.nix
+++ default.nix
@@ -2,13 +2,13 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "devshell-timelog";
-  version = "1.0.0";
+  version = "1.1.0";
 
   src = fetchFromGitHub {
     owner = "Aras14HD";
     repo = pname;
     rev = version;
-    hash = "sha256-Ib310d+kkN6kXXESJJmOgufuz3p1BOZrOuJYbDBdA6Y=";
+    hash = "sha256-Qwqp620tAtRdXI9uLNRACAV6vo6mzb7F2zouTNOas84=";
   };
 
   cargoLock = {
