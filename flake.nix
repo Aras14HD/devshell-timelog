acfd8ad1fb14cd545183a208773276f577b4b879
Aras14HD <aras14k5@gmail.com>
2024-09-09 11:36:03 +0200

    fix flake

--- flake.nix
+++ flake.nix
@@ -6,9 +6,7 @@
   outputs = { self, nixpkgs, flake-utils, ...}: flake-utils.lib.eachDefaultSystem (system:
     let pkgs = nixpkgs.legacyPackages.${system};
     in {
-      packages.hello = pkgs.callPackage ./default.nix {};
-      legacyPackages = packages;
-      defaultPackage = packages.hello;
+      defaultPackage = pkgs.callPackage ./default.nix {};
 
       devShell = pkgs.mkShell {
         packages = with pkgs; [
